5ced573a81c0d978af4e63b288eb767ae2b5e03c
Anubhav Munjaal <me@anubhavmunjaal.com>
2024-04-12 11:24:21 +0530

    added main file

---
+++ wp-menu-zapper.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: WP MENU ZAPPER
+Plugin URI: https://www.anubhavmunjaal.com/wp-menu-zapper
+Description: A Plugin made specifically to remove menus from WordPress Menu manager in bulk manner.
+Version: 1.0
+Author: Anubhav Munjaal
+Author URI: https://www.anubhavmunjaal.com
+License: GPL2
+*/
